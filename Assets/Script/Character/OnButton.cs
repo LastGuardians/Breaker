1e65094c422ee8a564a78d13695d43895b9bb271
smsychjy <smsychjy@snu.ac.kr>
2017-08-20 02:21:30 +0900

    캐릭터 선택창 구현중

---
+++ Assets/Script/Character/OnButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class OnButton : MonoBehaviour
+{
+    public UISprite TargetButton;
+    public string TargetCharacter;
+    public UISprite TargetSprite;
+    private UIAtlas TargetAtlas;
+
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                TargetSprite.atlas = Resources.Load("Atlas", typeof(UIAtlas)) as UIAtlas;
+                TargetSprite.spriteName = "select1";
+                Debug.Log(TargetSprite.spriteName);
+
+                GameObject CharacterButton = new GameObject(string.Format("Selection{0}", i));
+                CharacterButton.transform.position = new Vector3((j + 1) - 2, i - 1, 0);
+            }
+        }
+    }
+
+    void OnClick()
+    {
+        TargetButton = this.GetComponent<UISprite>();
+        if (TargetButton.name == "HomeButton")
+        {
+            TargetButton.enabled = false;
+            SceneManager.LoadScene("Main");
+        }
+        else
+        {
+            TargetCharacter = TargetButton.name.Substring(7, TargetButton.name.Length);
+        }
+    }
+}
+
+
