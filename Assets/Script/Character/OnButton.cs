fd704b0daddb197e4f83e4e3a521b49892ccf329
smsychjy <smsychjy@snu.ac.kr>
2017-08-21 00:43:43 +0900

    캐릭터 버튼 구현

--- Assets/Script/Character/OnButton.cs
+++ Assets/Script/Character/OnButton.cs
@@ -2,44 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 public class OnButton : MonoBehaviour
 {
-    public UISprite TargetButton;
-    public string TargetCharacter;
-    public UISprite TargetSprite;
-    private UIAtlas TargetAtlas;
+	public UISprite TargetButton;
+	public static string TargetCharacter;
 
-    void Start()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                TargetSprite.atlas = Resources.Load("Atlas", typeof(UIAtlas)) as UIAtlas;
-                TargetSprite.spriteName = "select1";
-                Debug.Log(TargetSprite.spriteName);
-
-                GameObject CharacterButton = new GameObject(string.Format("Selection{0}", i));
-                CharacterButton.transform.position = new Vector3((j + 1) - 2, i - 1, 0);
-            }
-        }
-    }
-
-    void OnClick()
-    {
-        TargetButton = this.GetComponent<UISprite>();
-        if (TargetButton.name == "HomeButton")
-        {
-            TargetButton.enabled = false;
-            SceneManager.LoadScene("Main");
-        }
-        else
-        {
-            TargetCharacter = TargetButton.name.Substring(7, TargetButton.name.Length);
-        }
-    }
+	void OnClick()
+	{
+		TargetButton = this.GetComponent<UISprite>();
+		if (TargetButton.name == "HomeButton")
+		{
+			TargetButton.enabled = false;
+			SceneManager.LoadScene("Main");
+		}
+		else
+		{
+			TargetCharacter = TargetButton.name;
+			SceneManager.LoadScene("CharacterSpecific");
+		}
+	}
 }
-
-
